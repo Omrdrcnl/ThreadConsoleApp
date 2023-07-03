d55813f45fbec2ffed4042a6bce669e147a8415c
Ömer DİRİCANLI <omrdrcnl@gmail.com>
2023-07-03 14:05:37 +0300

    TryCatchMethodAdd

--- ThreadConsoleApp/Program.cs
+++ ThreadConsoleApp/Program.cs
@@ -15,33 +15,46 @@ class program
         Console.WriteLine("Thread oluştıulacak txt dosya yolunu giriniz:");
         string dosyaYolu = Console.ReadLine();
         // StreamReader methodu okuma işlemi yapar.
-        using (StreamReader okuyucu = new StreamReader(dosyaYolu))
-        {
-            string kelime;
-            while ((kelime = okuyucu.ReadLine()) != null)
-            {
-                string[] kelimeler = kelime.Split(' ');
 
-                foreach (string s in kelimeler)
-                {   //Enqueune işlemi threada ekleme yapar
+        try 
+        {
+            using (StreamReader okuyucu = new StreamReader(dosyaYolu))
+            {
+                string kelime;
+                while ((kelime = okuyucu.ReadLine()) != null)
+                {
+
+                    string[] kelimeler = kelime.Split(' ');
+                    foreach (string s in kelimeler)
+                    {   //Enqueune işlemi threada ekleme yapar
                     ThreadKuyruk.Enqueue(s.Trim());
+                    }
                 }
             }
-        }
 
-        // threadları sırasıyla çalıştırma
-        Thread[] calisacakThread = new Thread[ThreadKuyruk.Count];
-        int threadIndex = 0;
-        foreach (string kelime in ThreadKuyruk)
-        {   
-            Console.WriteLine("Thread {0} Başarıyla çalıştı. İşlem Sonucu aşağıdadır.", threadIndex);
-            calisacakThread[threadIndex] = new Thread(() => KelimeFonksiyonu(kelime));
-            calisacakThread[threadIndex].Start();
-            Thread.Sleep(3000);
-            
-            threadIndex++;
+            // threadları sırasıyla çalıştırma
+            Thread[] calisacakThread = new Thread[ThreadKuyruk.Count];
+            int threadIndex = 1;
+            foreach (string kelime in ThreadKuyruk)
+            {   
+                Console.WriteLine("Thread {0} Başarıyla çalıştı. İşlem Sonucu aşağıdadır.", threadIndex);
+                calisacakThread[threadIndex-1] = new Thread(() => KelimeFonksiyonu(kelime));
+                calisacakThread[threadIndex-1].Start();
+                Thread.Sleep(3000);
+                threadIndex++;
+            }
+            //threadların tamamlanma işlemi join ile yapılıyor.
+            foreach (Thread thread in calisacakThread)
+            {
+                thread.Join();
+            }
+            Console.WriteLine("İşlem tamamlandı.");
         }
-
+        catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        
     }
     // threadların içinde çalışacak fonksiyon
     
